1b337305460e7b40c3090de5adae1000c216a20e
Sandro <sandrolp41@hotmail.com>
2025-11-26 23:11:16 -0300

    adiciona atributo

--- banco_rh.sql
+++ banco_rh.sql
@@ -6,14 +6,15 @@ CREATE TABLE colaborador (
 id_colaboradores INT PRIMARY KEY AUTO_INCREMENT, 
 nome VARCHAR(250) NOT NULL, 
 cargo VARCHAR(250) NOT NULL,
-salario DECIMAL(10,2) NOT NULL
+salario DECIMAL(10,2) NOT NULL,
+data_admissao DATE
 ); 	
 
-INSERT INTO colaborador(nome, cargo, salario) VALUES ("Ana", "Gerente", 8500.00);
-INSERT INTO colaborador(nome, cargo, salario) VALUES ("Bianca", "Coordenador", 6000.00);
-INSERT INTO colaborador(nome, cargo, salario) VALUES ("Carlos", "Estagiário", 1500.00);
-INSERT INTO colaborador(nome, cargo, salario) VALUES ("Daniel", "Analista", 4000.00);
-INSERT INTO colaborador(nome, cargo, salario) VALUES ("Elisa", "Estagiário", 1500.00);
+INSERT INTO colaborador(nome, cargo, salario, data_admissao) VALUES ("Ana", "Gerente", 8500.00, '2012-12-02');
+INSERT INTO colaborador(nome, cargo, salario, data_admissao) VALUES ("Bianca", "Coordenador", 6000.00, '2016-03-25');
+INSERT INTO colaborador(nome, cargo, salario, data_admissao) VALUES ("Carlos", "Estagiário", 1500.00, '2025-01-15');
+INSERT INTO colaborador(nome, cargo, salario, data_admissao) VALUES ("Daniel", "Analista", 4000.00, '2022-07-09');
+INSERT INTO colaborador(nome, cargo, salario, data_admissao) VALUES ("Elisa", "Estagiário", 1500.00, '2024-08-29');
 
 SELECT * FROM colaborador;
 
